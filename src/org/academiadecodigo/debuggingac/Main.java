9ec34f56b31e1ec70c3d468c892b6fea8b4bed34
DefKorns <defkorns@gmail.com>
2018-05-23 21:57:58 +0000

    menu

--- src/org/academiadecodigo/debuggingac/Main.java
+++ src/org/academiadecodigo/debuggingac/Main.java
@@ -1,7 +1,8 @@
 package org.academiadecodigo.debuggingac;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        Game game = new Game();
+        game.menu();
     }
 }
