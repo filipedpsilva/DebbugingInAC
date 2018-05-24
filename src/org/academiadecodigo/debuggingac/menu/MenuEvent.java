d78dba57053a6aa16c3c0a403b45bdd03880cb6a
Filipe <filipedpsilva@protonmail.com>
2018-05-24 19:06:54 +0000

    add menu event

--- src/org/academiadecodigo/debuggingac/menu/MenuEvent.java
+++ src/org/academiadecodigo/debuggingac/menu/MenuEvent.java
@@ -7,29 +7,54 @@ import org.academiadecodigo.debuggingac.simplegraphics.pictures.Picture;
 public class MenuEvent {
     private Picture menuBG;
     private Picture menuLoader;
+    private Picture menuLoader2;
     private Audio menu;
     private Audio loading;
 
 
     public MenuEvent() {
-        this.menuBG = new Picture(0, 0, "resources/images/menu/menu.png");
+
+        this.menuBG = new Picture(0, 0, "resources/images/menu/menu3.png");
         this.menuLoader = new Picture(0, 0, "resources/images/menu/loading.png");
+        this.menuLoader2 = new Picture(0,0,"resources/images/menu/loading2.png");
         this.loading = new Audio("/resources/sounds/boot.wav");
+
     }
 
     public void init() throws InterruptedException{
+
+
         menuLoader.draw();
-        Thread.sleep(400);
+        Thread.sleep(800);
+        menuLoader.delete();
+        menuLoader2.draw();
+        Thread.sleep(800);
+        menuLoader2.delete();
+        menuLoader.draw();
+        Thread.sleep(800);
+        menuLoader.delete();
+        menuLoader2.draw();
+        Thread.sleep(800);
         loading.start(true);
         Thread.sleep(1750);
-        menuLoader.delete();
+        menuLoader2.delete();
+
     }
     public void mainMenu(){
 
         menuBG.draw();
+
     }
     public void killSoundMenu(){
+
         menu.stop();
+
+    }
+
+    public void instructionsMenu() {
+
+
+
     }
 
     public void soundMenu(){
